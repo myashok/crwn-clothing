a67f461417de0554eb1d82970bdccae4752c8bd2
Shiv Pratap <iit2014121@iiita.ac.in>
2021-04-13 22:37:32 +0530

    added stripe for testing purpose

--- src/pages/checkoutPage/checkout.component.jsx
+++ src/pages/checkoutPage/checkout.component.jsx
@@ -8,6 +8,7 @@ import {
 } from '../../redux/cart/cart-selectors';
 
 import './checkout.styles.scss';
+import StripeCheckoutButton from '../../components/stripe/stripe-button.component';
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className='checkout-page'>
@@ -30,6 +31,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     </div>
     {cartItems.map(item => (<CheckoutItem key={item.id} cartItem={item} />))}
     <div className='total'>TOTAL: ${total}</div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/25 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
